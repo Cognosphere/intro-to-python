21da76c9d14a0e2e64694d294a2fb1750c846e3a
Rayan Sudeora <rayansudeora@gmail.com>
2020-08-07 18:32:23 +0000

    Added class 1 code

---
+++ main.py
@@ -0,0 +1,12 @@
+#What is a string?
+"Hello World"
+'Hello World'
+'My name is John'
+'I like coding'
+"John"
+
+#Introduce print statements
+print("Hello World")
+print("I like coding")
+print('I like coding')
+#print('Don't Give Up!)
